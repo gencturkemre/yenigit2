62f64d79565653083cea99cdd45e1dc101e90280
gencturkemre <you@example.com>
2022-11-02 23:24:31 +0300

    karisiklik giderildi

--- src/D01.java
+++ src/D01.java
@@ -1,5 +1,5 @@
 public class D01 {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("gitHub3 klasorune gectik");
     }
 }
--- src/E01.java
+++ src/E01.java
@@ -1,5 +1,26 @@
 public class E01 {
     public static void main(String[] args) {
+        System.out.println("main branch'de e01 de degisiklik yaptim");
+
+
+        System.out.println("deneme branch'inde degisiklik yaptik");
+
+        /*
+        Eger birden fazla branch ile calisiliyorsa
+        iki branch'de de degisiklikler yapilabilir
+        bu degisiklikler birbiriyle cakismiyorsa
+        git iki branch'de yapilan degisikliklerin tamamini korur ve
+        btranch'leri otomatik birlestirir
+        cakisma varsa, karar vermeniz icin cakisan yerleri CTE'li olarak verir
+        siz kodlari inceleyip fazlalik olarak git'in ekledigi yazilari silmeli,
+        cakisan kodlarin hangisinin kalip hangisinin silinecegine,
+        veya uygunsa ikisinin de kalmasina karar vermelisiniz
+         */
+
+
+        System.out.println("main branch e01 8.satirda degisiklik yaptim");
+
+        System.out.println("deneme branch e01 8.satir degisiklik");
 
     }
 }
